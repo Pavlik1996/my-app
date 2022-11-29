0e7bfd11668fa0e2fe37fd54c76136ffb7e60c53
Pavlik1996 <miki2012miki@gmail.com>
2022-11-29 14:30:58 +0300

    add post and user component

---
+++ src/Post/post.tsx
@@ -0,0 +1,8 @@
+
+export const Test = () => {
+    return (
+        <div>
+            <h2>Post</h2>
+        </div>
+    )
+}
---
+++ src/User/user.tsx
@@ -0,0 +1,7 @@
+export const Test = () => {
+    return (
+        <div>
+            <h2>User</h2>
+        </div>
+    )
+}
