c0f4dc8b2fb6a7d2b29425e1070cf859021966c5
Pavlik1996 <miki2012miki@gmail.com>
2022-11-29 14:23:09 +0300

    commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from "./Header";
 import Technologies from "./Technologies";
@@ -7,6 +6,7 @@ import Technologies from "./Technologies";
 const App = () => {
     return (
         <div>
+            <p>Lorem</p>
             <Header/>
             <Technologies/>
         </div>
